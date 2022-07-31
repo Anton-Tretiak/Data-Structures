951aa7407433b2323c494cf10c06aede61c1bbfd
Anton-Tretiak <105153156+Anton-Tretiak@users.noreply.github.com>
2022-07-31 15:21:16 +0300

    Linked List

--- Linked List.js
+++ Linked List.js
@@ -1,4 +1,4 @@
-class LinkedListNode {
+class LinkedListNode {            // Класс который создает ноду котрая содержит в себе знаение и ссылку на следующую ноду
   constructor(value, next = null) {
     this.value = value;
     this.next = next;
@@ -8,13 +8,13 @@ class LinkedListNode {
     return `${this.value}`;
   }
 }
-class LinkedList {
+class LinkedList {                // Класс который создает сам связный список
   constructor() {
     this.head = null;
     this.tail = null;
   }
 
-  append(value) {
+  append(value) {               
     const newNode = new LinkedListNode(value);
     if (!this.head || !this.tail) {
       this.head = newNode;
@@ -28,6 +28,64 @@ class LinkedList {
 
     return this;
   }
+
+  prepend(value) {
+    const newNode = new LinkedListNode(value, this.head);
+    this.head = newNode;
+    if (!this.tail) {
+      this.tail = newNode;
+    }
+
+    return this;
+  }
+
+  find(value) {
+    if (!this.head) {
+      return null;
+    }
+    let currentNode = this.head;
+
+    while(currentNode) {
+      if (currentNode.value === value) {
+        return currentNode;
+      }
+
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
+
+  delete(value) {
+    if (!this.head) {
+      return null;
+    }
+    let deletedNode = null;
+    while (this.head && this.head.value === value) {
+      deletedNode = this.head;
+
+      this.head = this.head.next;
+    }
+
+    let currentNode = this.head;
+
+    if (currentNode !== null) {
+      while (currentNode.next) {
+        if (currentNode.next.value === value) {
+          deletedNode = currentNode.next;
+          currentNode.next = currentNode.next.next;
+        }
+        else {
+          currentNode = currentNode.next;
+        }
+      }
+    }
+     if (this.tail?.value === value) {
+      this.tail = currentNode;
+     }
+
+     return deletedNode;
+  }
+
   toArray() {
     const nodes = [];
     let currentNode = this.head;
@@ -36,6 +94,7 @@ class LinkedList {
       nodes.push(currentNode);
       currentNode = currentNode.next;
     }
+
     return nodes
   }
   toString() {
@@ -44,6 +103,14 @@ class LinkedList {
 }
 
 const list = new LinkedList();
-list.append('a').append('b').append('c');
+list.append('a').append('b').append('c');   // Добавляет новуую ноду в конец списка
 
-console.log(list.toString());
+console.log(list);
+console.log(list.toArray());                // Переводит список в массив
+console.log(list.toString());               // Переводит элементы ноды в строку
+
+list.prepend('r').prepend('k');             // Добавление новой лист ноды в начало списка
+
+console.log(list.find('b'));                // Поиск элемента в лист ноде
+list.delete('c');                           // Удаление ноды со списка со значением внутри метода
+console.log(list);
